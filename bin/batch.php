4e7b5d3db6916ab7328bf6a5083b6da737cc88f0
Pedro Pelaez <aaaaa976@gmail.com>
2015-03-12 23:33:00 +0100

    Command to execute a serial of commands.

---
+++ bin/batch.php
@@ -0,0 +1,78 @@
+<?php
+
+/* 
+ * Copyright (C) 2015 Pedro Pelaez <aaaaa976@gmail.com>
+ * Sources https://github.com/PSF1/pharinix
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+if (!defined("CMS_VERSION")) { header("HTTP/1.0 404 Not Found"); die(""); }
+
+if (!class_exists("commandBatch")) {
+    class commandBatch extends driverCommand {
+
+        public static function runMe(&$params, $debug = true) {
+            $params = array_merge(array(
+                "starter" => array(),
+                "commands" => array(),
+                "echoed" => "",
+            ), $params);
+            
+            $lastCommand = "";
+            $echoed = $params["echoed"];
+            try {
+                $output = is_array($params["starter"])?$params["starter"]:array();
+                foreach($params["commands"] as $cmd => $params) {
+                    $lastCommand = "'$cmd' => '$params'";
+                    $aux = array();
+                    parse_str($params, $aux);
+                    $params = array_merge($output, $aux);
+                    unset($aux);
+                    $output = array_merge($output, driverCommand::run($cmd, $params));
+                }
+            } catch (Exception $exc) {
+                $output["ok"] = false;
+                $output["msg"] = $exc->getMessage();
+                $output["error"] = $lastCommand;
+            }
+            if (!is_string($echoed)) {
+                $echoed = "";
+            }
+            if ($echoed != "") {
+                driverCommand::run($echoed, $output);
+            } else {
+                return $output;
+            }
+         }
+
+        public static function getHelp() {
+            return array(
+                "description" => "Execute a serial of commands. The output of a command will be the input parameters of the next command.", 
+                "parameters" => array(
+                    "starter" => "Array of params to merge at the start.",
+                    "commands" => "Array of commands and default parameters. This parameters, if any, will be merged with de output of previous command, with priority to this, and pased how combined parameters. Ex, without '[', ']': ['nothing' => 'ignoredparam1=A&ignoredparam2=B', 'nothing' => 'ignoredparam1=A&ignoredparam2=B', ...]",
+                    "echoed" => "If is empty or not define the output will out in responde, else will be pased how parameters to the command in this parameter. Ex: 'echoed' => 'toJson' will echo to the browser a json representation of the response.",
+                ), 
+                "response" => array(
+                    "any" => "The final response of the batch.",
+                    "ok" => "If error is set to FALSE, else will be unset.",
+                    "msg" => "If error is set to the error message, else will be unset. ",
+                    "error" => "If error is set to a string with the command and her input parameters, else will be unset.",
+                )
+            );
+        }
+    }
+}
+return new commandBatch();
