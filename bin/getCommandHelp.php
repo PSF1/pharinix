bc4d0bd144b864de1762b71c208b589024c8fd56
Pedro Pelaez <aaaaa976@gmail.com>
2015-04-18 19:58:54 +0200

    getCommandHelp ignore security files.

--- bin/getCommandHelp.php
+++ bin/getCommandHelp.php
@@ -29,7 +29,7 @@ if (!class_exists("commandGetCommandHelp")) {
             $q = dbConn::Execute($sql);
             while(!$q->EOF) {
                 $resp["help"][$q->fields["path"]] = array();
-                $cmds = driverTools::lsDir($q->fields["path"]);
+                $cmds = driverTools::lsDir($q->fields["path"], "*.php");
                 foreach($cmds["files"] as $cmd) {
                     $cmd = str_replace($q->fields["path"], "", $cmd);
                     $cmd = str_replace(".php", "", $cmd);
