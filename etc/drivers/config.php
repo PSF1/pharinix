36f8b3595dddb41b876a7a59b533e88f22340ab9
Pedro Pelaez <aaaaa976@gmail.com>
2015-06-28 01:21:02 +0200

    INI files parser with tests.

--- etc/drivers/config.php
+++ etc/drivers/config.php
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-define("CMS_VERSION", "1.05.15");
+define("CMS_VERSION", "1.06.28");
 @header("author: Pedro Pelaez <aaaaa976@gmail.com>");
 @header("generator: Pharinix/".CMS_VERSION);
 
@@ -37,5 +37,304 @@ class driverConfig {
             }
         }
         return "etc/pharinix.config.php";
+    }
+}
+
+/**
+ * Ini files parser
+ */
+class driverConfigIni {
+    const LEX_MODE_COMMENT = 'comment';
+    const LEX_MODE_STRING_VALUE = 'string_value';
+    const LEX_MODE_STRING_SINGLE_VALUE = 'string_single_value';
+    const LEX_MODE_VALUE = 'value';
+    const LEX_MODE_SECTION_NAME = 'section_name';
+    const LEX_MODE_KEY = 'key';
+    
+    /**
+     * When this value is TRUE all the methods return false.
+     * @var boolean 
+     */
+    protected $withError = false;
+    protected $errorMsg = '';
+    /**
+     * File to read/write
+     * @var string
+     */
+    protected $file = "";
+    /**
+     * INI raw content
+     * @var string
+     */
+    protected $fileContent = "";
+    protected $lexMaxInd = 0;
+    protected $lexInd = 0;
+    
+    protected $sections;
+    
+    public function __construct($file) {
+        if (!is_file($file)) {
+            $this->withError = true;
+            $this->errorMsg = 'File not exist.';
+            return;
+        }
+        $this->file = $file;
+        $this->fileContent = file_get_contents($this->file);
+        $this->lexInd = 0;
+        $this->lexMaxInd = strlen($this->fileContent);
+        //
+        $this->sections = array(
+            ' ' => new driverConfigIniSection(' '),
+        );
+    }
+    
+    public function getError() {
+        return $this->errorMsg;
+    }
+    
+    public function &getSections() {
+        return $this->sections;
+    }
+    
+    public function getSectionsNames() {
+        return array_keys($this->sections);
+    }
+    
+    /**
+     * Return a section
+     * @param string $name Section name. Start with '[' and end with ']'
+     * @return driverConfigIniSection
+     */
+    public function getSection($name) {
+        if (array_key_exists($name, $this->sections)) {
+            return $this->sections[$name];
+        } else {
+            return null;
+        }
+    }
+    
+    /**
+     * Parse the file to memory
+     */
+    public function parse() {
+        if ($this->withError) {
+            return false;
+        }
+        $activeSection = $this->sections[' '];
+        $waitingKey = true;
+        $lastKey = '';
+        $line = $this->lex();
+        while ($line !== false){
+            if (driverTools::str_start("[", $line)) {
+                if (!array_key_exists($line, $this->sections)) {
+                    $activeSection = new driverConfigIniSection();
+                    $activeSection->setName($line);
+                    $this->sections[$line] = $activeSection;
+                }
+                $activeSection = $this->sections[$line];
+                $waitingKey = true;
+            } else if (driverTools::str_start(";", $line)) {
+                $activeSection->add($line);
+                $waitingKey = true;
+            } else {
+                if ($waitingKey) {
+                    if ($line == "=") {
+                        $this->withError = true;
+                        $this->errorMsg = '"=" not espected.';
+                        return false;
+                    } else {
+                        $lastKey = $line;
+                        $waitingKey = false;
+                    }
+                } else {
+                    if ($line != '=') {
+                        if ($lastKey == '') {
+                            $this->withError = true;
+                            $this->errorMsg = 'Value without key.';
+                            return false;
+                        } else {
+                            $activeSection->set($lastKey, $line);
+                            $lastKey = '';
+                            $waitingKey = true;
+                        }
+                    }
+                }
+            }
+            $line = $this->lex();
+        };
+    }
+    
+    /**
+     * Return a token or false at the end
+     * @return string or false
+     */
+    public function lex() {
+        if ($this->withError) {
+            return false;
+        }
+        $mode = self::LEX_MODE_KEY;
+        $resp = '';
+        while ($this->lexInd < $this->lexMaxInd) {
+            $c = substr($this->fileContent, $this->lexInd, 1);
+            switch($c) {
+                case ';': // Comment
+                    if ($mode == self::LEX_MODE_COMMENT || $mode == self::LEX_MODE_STRING_VALUE || $mode == self::LEX_MODE_STRING_SINGLE_VALUE) {
+                        $resp .= $c;
+                        continue;
+                    }
+                    $mode = self::LEX_MODE_COMMENT;
+                    if ($resp != '') {
+                        return $resp;
+                    }
+                    $resp .= ';';
+                    break;
+                case "'": // String value
+                    if ($mode == self::LEX_MODE_STRING_SINGLE_VALUE) {
+                        return "'".$resp."'";
+                    }
+                    if ($mode != self::LEX_MODE_STRING_VALUE) {
+                        $mode = self::LEX_MODE_STRING_SINGLE_VALUE;
+                    } else {
+                        $resp .= $c;
+                    }
+                    break;
+                case '"': // String value
+                    if ($mode == self::LEX_MODE_STRING_VALUE) {
+                        return '"'.$resp.'"';
+                    }
+                    if ($mode != self::LEX_MODE_STRING_SINGLE_VALUE) {
+                        $mode = self::LEX_MODE_STRING_VALUE;
+                    } else {
+                        $resp .= $c;
+                    }
+                    break;
+                case '['; // Section name
+                    if ($mode == self::LEX_MODE_KEY) {
+                        $mode = self::LEX_MODE_SECTION_NAME;
+                    } else {
+                        $resp .= $c;
+                    }
+                    break;
+                case ']':
+                    if ($mode == self::LEX_MODE_SECTION_NAME) {
+                        ++$this->lexInd;
+                        return "[$resp]";
+                    }
+                    $resp .= $c;
+                    break;
+                case "\n": // New line
+                    if ($mode == self::LEX_MODE_COMMENT) {
+                        return $resp;
+                    } else if ($mode == self::LEX_MODE_SECTION_NAME) {
+                        
+                    } else if ($mode == self::LEX_MODE_STRING_VALUE || $mode == self::LEX_MODE_STRING_SINGLE_VALUE) {
+                        $resp .= $c;
+                    } else if ($mode == self::LEX_MODE_KEY) {
+                        if ($resp != '') return $resp;
+                    }
+                    break;
+                case '=':
+                    if ($mode != self::LEX_MODE_KEY) {
+                        $resp .= $c;
+                    } else {
+                        if (trim($resp) == '') {
+                            ++$this->lexInd;
+                            return '=';
+                        }
+                        return $resp;
+                    }
+                    break;
+                default:
+                    if ($mode != self::LEX_MODE_KEY || $c != ' ') {
+                        $resp .= $c;
+                    }
+                    break;
+            }
+            ++$this->lexInd;
+        }
+        if (trim($resp) != '') {
+            return $resp;
+        }
+        return false;
+    }
+}
+
+class driverConfigIniComment {
+    public $line;
+}
+
+class driverConfigIniPair {
+    public $key;
+    public $value;
+}
+
+class driverConfigIniSection {
+    protected $name;
+    protected $lines = array();
+    protected $keys = array();
+    
+    public function __construct($name = null) {
+        $this->name = $name;
+    }
+    
+    public function &getLines() {
+        return $this->lines;
+    }
+    
+    public function getName() {
+        return $this->name;
+    }
+    
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function get($key) {
+        if (array_key_exists($key, $this->keys)) {
+            $val = $this->keys[$key]->value;
+            if (driverTools::str_start("'", $val) || driverTools::str_start('"', $val)) {
+                $val = substr($val, 1, strlen($val)-2);
+            }
+            return $val;
+        } else {
+            return null;
+        }
+    }
+    
+    public function set($key, $value) {
+        if (array_key_exists($key, $this->keys)) {
+            $this->keys[$key]->value = $value;
+        } else {
+            $pair = new driverConfigIniPair();
+            $pair->key = $key;
+            $pair->value = $value;
+            $this->add($pair);
+        }
+    }
+    
+    public function add(&$line) {
+        $this->lines[] = $line;
+        if ($line instanceof driverConfigIniPair) {
+            $this->keys[$line->key] = $line;
+        }
+    }
+    
+    public function del($line) {
+        $max = count($this->lines);
+        for($i = 0; $i < $max; ++$i) {
+            $ref = $this->lines[$i];
+            if ($line instanceof driverConfigIniComment) {
+                if ($ref->line == $line->line) {
+                    unset($this->lines[$i]);
+                    return;
+                }
+            } if ($line instanceof driverConfigIniPair) {
+                if ($ref->key == $line->key) {
+                    unset($this->lines[$i]);
+                    unset($this->keys[$line->key]);
+                    return;
+                }
+            }
+        }
     }
 }
---
+++ tests/drivers/commandTest/iniFilesTest.php
@@ -0,0 +1,326 @@
+<?php
+
+/* 
+ * Copyright (C) 2015 Pedro Pelaez <aaaaa976@gmail.com>
+ * Sources https://github.com/PSF1/pharinix
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+class iniFilesTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp() {
+        while (!is_file("etc/pharinix.config.php")) {
+            chdir("../");
+        }
+        include_once 'tests/drivers/etc/bootstrap.php';
+        //driverUser::sessionStart();
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown() {
+        //driverUser::logOut();
+    }
+    
+    public function testLexEmpty() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/empty.ini');
+        $token = $cfg->lex();
+        $this->assertEquals('', $cfg->getError());
+        $this->assertFalse($token);
+    }
+    
+    public function testLexStartNewLine() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/start_new_line.ini');
+        $ind = 0;
+        $resps = array(
+            "; this is an INI file",
+            "[section]",
+            "key",
+            "=",
+            "value",
+            false
+        );
+        do {
+            $token = $cfg->lex();
+            $this->assertEquals($resps[$ind], $token);
+            ++$ind;
+        } while ($token !== false);
+    }
+    
+    public function testLexMultiLineValue() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/multiline_value.ini');
+        $ind = 0;
+        $resps = array(
+            "; this is an INI file",
+            "[section]",
+            "key",
+            "=",
+            "\"value value value value value value \nvalue value value value value \"",
+            false
+        );
+        do {
+            $token = $cfg->lex();
+            $this->assertEquals($resps[$ind], $token);
+            ++$ind;
+        } while ($token !== false);
+    }
+    
+    public function testLexMultiLineSingleQuoteValue() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/multiline_value_simple_quote.ini');
+        $ind = 0;
+        $resps = array(
+            "; this is an INI file",
+            "[section]",
+            "key",
+            "=",
+            "'value value value value value value \nvalue value value value value '",
+            false
+        );
+        do {
+            $token = $cfg->lex();
+            $this->assertEquals($resps[$ind], $token);
+            ++$ind;
+        } while ($token !== false);
+    }
+    
+    public function testLexMultiLineWithSingleQuote() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/multiline_value_with_single_quote.ini');
+        $ind = 0;
+        $resps = array(
+            "; this is an INI file",
+            "[section]",
+            "key",
+            "=",
+            "\"value 'value' value value value value \nvalue value value value value \"",
+            false
+        );
+        do {
+            $token = $cfg->lex();
+            $this->assertEquals($resps[$ind], $token);
+            ++$ind;
+        } while ($token !== false);
+    }
+    
+    public function testLexNoValue() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/no_value.ini');
+        $ind = 0;
+        $resps = array(
+            "; this is an INI file",
+            "[section]",
+            "key",
+            "key2",
+            "=",
+            "value",
+            false
+        );
+        do {
+            $token = $cfg->lex();
+            $this->assertEquals($resps[$ind], $token);
+            ++$ind;
+        } while ($token !== false);
+    }
+    
+    public function testLexCommentAtEnd() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/comment_at_end.ini');
+        $ind = 0;
+        $resps = array(
+            "; this is an INI file",
+            "[section]",
+            "key",
+            "=",
+            "1",
+            "; A comment",
+            "key1",
+            "=",
+            "; Other comment",
+            false
+        );
+        do {
+            $token = $cfg->lex();
+            $this->assertEquals($resps[$ind], $token);
+            ++$ind;
+        } while ($token !== false);
+    }
+    
+    public function testLexCommentFakeInValue() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/comment_fake_in_value.ini');
+        $ind = 0;
+        $resps = array(
+            "; this is an INI file",
+            "[section]",
+            "key",
+            "=",
+            "\"1 ; A comment\nkey = ; Other comment\"",
+            false
+        );
+        do {
+            $token = $cfg->lex();
+            $this->assertEquals($resps[$ind], $token);
+            ++$ind;
+        } while ($token !== false);
+    }
+    
+    public function testLexEmptyValue() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/empty_value.ini');
+        $ind = 0;
+        $resps = array(
+            "key",
+            "=",
+            "''",
+            false
+        );
+        do {
+            $token = $cfg->lex();
+            $this->assertEquals($resps[$ind], $token);
+            ++$ind;
+        } while ($token !== false);
+    }
+    
+    public function testLexDuplicateKey() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/duplicated_key.ini');
+        $ind = 0;
+        $resps = array(
+            "; this is an INI file",
+            "[section]",
+            "key",
+            "=",
+            "1",
+            "key",
+            "=",
+            "2",
+            false
+        );
+        do {
+            $token = $cfg->lex();
+            $this->assertEquals($resps[$ind], $token);
+            ++$ind;
+        } while ($token !== false);
+    }
+    
+    public function testParse_comment_at_end() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/comment_at_end.ini');
+        $cfg->parse();
+        $this->assertEquals('1', $cfg->getSection('[section]')->get('key'));
+        $this->assertEquals('', $cfg->getSection('[section]')->get('key1'));
+    }
+    
+    public function testParse_comment_fake_in_value() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/comment_fake_in_value.ini');
+        $cfg->parse();
+        $this->assertEquals("1 ; A comment\nkey = ; Other comment", $cfg->getSection('[section]')->get('key'));
+    }
+    
+    public function testParse_duplicated_key() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/duplicated_key.ini');
+        $cfg->parse();
+        $this->assertEquals("2", $cfg->getSection('[section]')->get('key'));
+    }
+    
+    public function testParse_empty() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/empty.ini');
+        $cfg->parse();
+        $this->assertTrue(true);
+    }
+    
+    public function testParse_empty_section() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/empty_section.ini');
+        $cfg->parse();
+        $this->assertNotNull($cfg->getSection('[section]'));
+    }
+    
+    public function testParse_empty_value() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/empty_value.ini');
+        $cfg->parse();
+        $this->assertEquals('', $cfg->getSection(' ')->get('key'));
+    }
+    
+    public function testParse_free_key() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/free_key.ini');
+        $cfg->parse();
+        $this->assertEquals('value', $cfg->getSection(' ')->get('key'));
+    }
+    
+    public function testParse_multiline_value() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/multiline_value.ini');
+        $cfg->parse();
+        $this->assertEquals("value value value value value value \nvalue value value value value ", $cfg->getSection('[section]')->get('key'));
+    }
+    
+    public function testParse_multiline_value_simple_quote() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/multiline_value_simple_quote.ini');
+        $cfg->parse();
+        $this->assertEquals("value value value value value value \nvalue value value value value ", $cfg->getSection('[section]')->get('key'));
+    }
+    
+    public function testParse_multiline_value_with_single_quote() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/multiline_value_with_single_quote.ini');
+        $cfg->parse();
+        $this->assertEquals("value 'value' value value value value \nvalue value value value value ", $cfg->getSection('[section]')->get('key'));
+    }
+    
+    public function testParse_no_key() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/no_key.ini');
+        $cfg->parse();
+        $this->assertEquals("", $cfg->getSection('[section]')->get('value'));
+    }
+    
+    public function testParse_no_value() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/no_value.ini');
+        $cfg->parse();
+        $this->assertEquals("key2", $cfg->getSection('[section]')->get('key'));
+        $this->assertEquals('"=" not espected.', $cfg->getError());
+    }
+    
+    public function testParse_php_head() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/php_head.ini');
+        $cfg->parse();
+        $this->assertEquals("value", $cfg->getSection('[section]')->get('key'));
+    }
+    
+    public function testParse_start_new_line() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/start_new_line.ini');
+        $cfg->parse();
+        $this->assertEquals("value", $cfg->getSection('[section]')->get('key'));
+    }
+    
+    public function testParse_string_value() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/string_value.ini');
+        $cfg->parse();
+        $this->assertEquals("value", $cfg->getSection('[section]')->get('key'));
+    }
+    
+    public function testParse_two_keys() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/two_keys.ini');
+        $cfg->parse();
+        $this->assertEquals("1", $cfg->getSection('[section]')->get('key'));
+        $this->assertEquals("2", $cfg->getSection('[section]')->get('key1'));
+        $this->assertEquals("3", $cfg->getSection('[section]')->get('key2'));
+        $this->assertEquals("4", $cfg->getSection('[section]')->get('key3'));
+    }
+    
+    public function testParse_two_sections() {
+        $cfg = new driverConfigIni('tests/drivers/cfg_ini/two_sections.ini');
+        $cfg->parse();
+        $this->assertEquals("value", $cfg->getSection('[section]')->get('key'));
+        $this->assertEquals("value", $cfg->getSection('[section 2]')->get('key'));
+    }
+}
