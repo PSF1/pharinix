672cb6d18aebf272e2880a4b032a45e5d356594b
Pedro Pelaez <aaaaa976@gmail.com>
2015-12-27 13:37:33 +0100

    Command to obfuscate a password.

---
+++ bin/obfPass.php
@@ -0,0 +1,81 @@
+<?php
+
+/* 
+ * Pharinix Copyright (C) 2015 Pedro Pelaez <aaaaa976@gmail.com>
+ * Sources https://github.com/PSF1/pharinix
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+if (!defined("CMS_VERSION")) { header("HTTP/1.0 404 Not Found"); die(""); }
+
+if (!class_exists("commandObfPass")) {
+    class commandObfPass extends driverCommand {
+
+        public static function runMe(&$params, $debug = true) {
+            $params = array_merge(array(
+                'pass' => uniqid("ph", true),
+            ), $params);
+            return array(
+                'pass' => $params['pass'],
+                'obf' => driverUser::passwordObfuscation($params['pass'])
+            );
+        }
+
+        public static function getHelp() {
+            return array(
+                "package" => 'core',
+                "description" => __("Obfuscate a password."), 
+                "parameters" => array(
+                    'pass' => __('Password to obfuscate.'),
+                ), 
+                "response" => array(
+                    'pass' => __('Raw password.'),
+                    'obf' => __('Obfuscated password.')
+                ),
+                "type" => array(
+                    "parameters" => array(
+                        'pass' => 'string'
+                    ), 
+                    "response" => array(
+                        'pass' => 'string',
+                        'obf' => 'string'
+                    ),
+                ),
+                "echo" => false,
+                "interface" => false,
+//                "hooks" => array(
+//                        array(
+//                            "name" => "nothingHook",
+//                            "description" => "sadasdaAllow rewrite a HTML alert message.",
+//                            "parameters" => array(
+//                                "alert" => "asdasdResponse to be echoed to the client.",
+//                                "msg" => "asdasdThe message to show."
+//                            )
+//                        )
+//                )
+            );
+        }
+        
+        public static function getAccess($ignore = "") {
+            $me = __FILE__;
+            return parent::getAccess($me);
+        }
+        
+        public static function getAccessFlags() {
+            return driverUser::PERMISSION_FILE_ALL_EXECUTE;
+        }
+    }
+}
+return new commandObfPass();
