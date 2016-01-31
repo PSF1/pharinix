600e213b40aee03cea8112ad33bc21b4d736bb9c
Pedro Pelaez <aaaaa976@gmail.com>
2016-01-31 01:36:32 +0100

    New command logTail to display logs. Thanks to Lorenzo Stanco - https://gist.github.com/lorenzos

---
+++ bin/logTail.php
@@ -0,0 +1,72 @@
+<?php
+
+/* 
+ * Pharinix Copyright (C) 2016 Pedro Pelaez <aaaaa976@gmail.com>
+ * Sources https://github.com/PSF1/pharinix
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+if (!defined("CMS_VERSION")) { header("HTTP/1.0 404 Not Found"); die(""); }
+
+if (!class_exists("commandLogTail")) {
+    class commandLogTail extends driverCommand {
+
+        public static function runMe(&$params, $debug = true) {
+            $params = array_merge(array(
+                    'path' => '',
+                    'n' => 25,
+                ), $params);
+            $resp = '';
+            
+            if (is_file($params['path'])) {
+                $resp = driverTools::tailCustom($params['path'], $params['n']);
+            } else {
+                $resp = array('ok' => false, 'msg' => __('File not found.'));
+            }
+            
+            return $resp;
+        }
+
+        public static function getAccess($ignore = "") {
+            $me = __FILE__;
+            return parent::getAccess($me);
+        }
+        
+//        public static function getAccessFlags() {
+//            return driverUser::PERMISSION_FILE_ALL_EXECUTE;
+//        }
+        
+        public static function getHelp() {
+            return array(
+                "package" => 'core',
+                "description" => __("Output the last part of file."), 
+                "parameters" => array(
+                    'path' => __('File path.'),
+                    'n' => __('Number of lines, default 25.'),
+                ), 
+                "response" => array(),
+                "type" => array(
+                    "parameters" => array(
+                        'path' => 'string',
+                        'n' => 'integer',
+                    ), 
+                    "response" => array(),
+                ),
+                "echo" => false
+            );
+        }
+    }
+}
+return new commandLogTail();
