f78985c9b3931c34297bdaa71ee14a1cbdbfb8a4
Pedro Pelaez <aaaaa976@gmail.com>
2016-01-31 01:46:08 +0100

    New command logClear to empty logs.

---
+++ bin/logClear.php
@@ -0,0 +1,76 @@
+<?php
+
+/* 
+ * Pharinix Copyright (C) 2016 Pedro Pelaez <aaaaa976@gmail.com>
+ * Sources https://github.com/PSF1/pharinix
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+if (!defined("CMS_VERSION")) { header("HTTP/1.0 404 Not Found"); die(""); }
+
+if (!class_exists("commandLogClear")) {
+    class commandLogClear extends driverCommand {
+
+        public static function runMe(&$params, $debug = true) {
+            $params = array_merge(array(
+                    'path' => ''
+                ), $params);
+            $resp = '';
+            
+            if (is_file($params['path'])) {
+                try {
+                    $handle = fopen($params['path'], "wb+");
+                    fwrite($handle, "");
+                    fclose($handle);
+                    $resp = array('ok' => true);
+                } catch (Exception $exc) {
+                    $resp = array('ok' => false, 'msg' => $exc->getMessage());
+                }
+            } else {
+                $resp = array('ok' => false, 'msg' => __('File not found.'));
+            }
+            
+            return $resp;
+        }
+
+        public static function getAccess($ignore = "") {
+            $me = __FILE__;
+            return parent::getAccess($me);
+        }
+        
+//        public static function getAccessFlags() {
+//            return driverUser::PERMISSION_FILE_ALL_EXECUTE;
+//        }
+        
+        public static function getHelp() {
+            return array(
+                "package" => 'core',
+                "description" => __("Clear a file."), 
+                "parameters" => array(
+                    'path' => __('File path.')
+                ), 
+                "response" => array(),
+                "type" => array(
+                    "parameters" => array(
+                        'path' => 'string'
+                    ), 
+                    "response" => array(),
+                ),
+                "echo" => false
+            );
+        }
+    }
+}
+return new commandLogClear();
