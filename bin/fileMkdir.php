363276431f26772a8150e91755418466952d197a
Pedro Pelaez <aaaaa976@gmail.com>
2016-03-05 19:35:05 +0100

    Added command fileMkdir to allow create folders in server.

---
+++ bin/fileMkdir.php
@@ -0,0 +1,89 @@
+<?php
+
+/* 
+ * Pharinix Copyright (C) 2015 Pedro Pelaez <aaaaa976@gmail.com>
+ * Sources https://github.com/PSF1/pharinix
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+if (!defined("CMS_VERSION")) { header("HTTP/1.0 404 Not Found"); die(""); }
+
+if (!class_exists("commandFileMkdir")) {
+    class commandFileMkdir extends driverCommand {
+
+        public static function runMe(&$params, $debug = true) {
+            $params = array_merge(array(
+                'path' => ''
+            ), $params);
+            
+            $resp = array('ok' => false, 'msg' => __('Path is required.'));
+            if ($params['path'] != '') {
+                $err = false;
+                $fInfo = driverTools::pathInfo($params['path']);
+                if (!$fInfo["exists"]) {
+                    $ok = @mkdir($params['path']);
+                    if (!$ok) {
+                        $resp['msg'] = __('Error creating directory.');
+                    } else {
+                        $resp = array('ok' => true);
+                        unset($resp['msg']);
+                    }
+                } else {
+                    $resp['msg'] = __('Path already exist.');
+                }
+            }
+            return $resp;
+        }
+
+        public static function getHelp() {
+            return array(
+                "package" => 'core',
+                "description" => __("Create a new folder in server."), 
+                "parameters" => array(
+                    'path' => __('Path to create.')
+                ), 
+                "response" => array(),
+                "type" => array(
+                    "parameters" => array(
+                        'path' => 'string'
+                    ), 
+                    "response" => array(),
+                ),
+                "echo" => false,
+                "interface" => false,
+//                "hooks" => array(
+//                        array(
+//                            "name" => "nothingHook",
+//                            "description" => "sadasdaAllow rewrite a HTML alert message.",
+//                            "parameters" => array(
+//                                "alert" => "asdasdResponse to be echoed to the client.",
+//                                "msg" => "asdasdThe message to show."
+//                            )
+//                        )
+//                )
+            );
+        }
+        
+        public static function getAccess($ignore = "") {
+            $me = __FILE__;
+            return parent::getAccess($me);
+        }
+        
+//        public static function getAccessFlags() {
+//            return driverUser::PERMISSION_FILE_ALL_EXECUTE;
+//        }
+    }
+}
+return new commandFileMkdir();
